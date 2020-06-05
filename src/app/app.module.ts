f210745318d0532001bbd23b248cb0088a9bb287
Shrujan Shetty <shrujan.shetty1@ibm.com>
2020-06-05 14:46:46 +0530

    finished code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { GridComponent } from './grid/grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule,
